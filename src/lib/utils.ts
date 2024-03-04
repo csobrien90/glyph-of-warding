b646fb50ae557cb3186fd30dff553abeb4a56c55
Chad OBrien <obrien.music@gmail.com>
2024-03-03 23:32:24 -0500

    stop fetch call from caching

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,9 @@ import { IpData, IpDataError } from "../types"
 
 export const getIpData = async (apiKey: string): Promise<IpData | IpDataError> => {
 	try {
-		const response = await fetch(`https://api.ipdata.co?api-key=${apiKey}`)
+		const response = await fetch(`https://api.ipdata.co?api-key=${apiKey}`, {
+			cache: 'no-store'
+		})
 		const json = await response.json() as IpData | IpDataError
 		return json
 	} catch (e) {
